cd3e169b4d81920c29d8294144ffa5dde252e363
unknown <nuruddeensalihu@yahoo.com>
2015-07-23 13:26:56 +0800

    Adding a new class GameEngine that oversees the general controller of the game. The class controls which key is pressed and what action to call. The class is the main game controller

---
+++ 2048_Console/gameEngine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048_Console
+{
+    class gameEngine : gameManager
+    {
+        private int[][] Tiles;
+        private DrawGame draw;
+        public gameEngine(int[][] tiles):base(tiles)
+        {
+            Tiles = tiles;
+            draw = new DrawGame(tiles);
+        }
+        public override void runGame(KeyCode k)
+        {
+            ConsoleKeyInfo cki;
+            KeyCode key;
+            // Prevent example from ending if CTL+C is pressed.
+            Console.TreatControlCAsInput = true;
+
+            Tiles = draw.WriteDefault();
+            draw.writeTiles(Tiles);
+
+            cki = Console.ReadKey();
+
+            if (cki.Key == ConsoleKey.Enter)
+            {
+                GameModel.isAuthorized = true;
+                GameModel.isGameOver = false;
+                GameModel.Score = 0;
+                draw.writeTiles(Tiles);
+                Console.WriteLine("Game Begins.\n");
+            }
+            else
+            {
+                Console.Write("You dint press the enter button\n");
+                cki = Console.ReadKey();
+                while (cki.Key != ConsoleKey.Escape)
+                {
+                    if (cki.Key == ConsoleKey.Enter)
+                    {
+                        GameModel.isAuthorized = true;
+                        GameModel.isGameOver = false;
+                        GameModel.Score = 0;
+                        Console.Write("Game Begins\n");
+                        cki = Console.ReadKey();
+                        break;
+                    }
+                    Console.Write("You dint press the enter button\n");
+                    cki = Console.ReadKey();
+                }
+            }
+            cki = Console.ReadKey();
+            do
+            {
+                if (cki.Key == ConsoleKey.DownArrow)
+                {
+                    base.runGame(KeyCode.Down);
+                    cki = Console.ReadKey();
+                }
+                else if (cki.Key == ConsoleKey.UpArrow)
+                {
+                    base.runGame(KeyCode.Up);
+                    cki = Console.ReadKey();
+                }
+                else if (cki.Key == ConsoleKey.LeftArrow)
+                {
+                    base.runGame(KeyCode.Left);
+                    cki = Console.ReadKey();
+                }
+                else if (cki.Key == ConsoleKey.RightArrow)
+                {
+                    base.runGame(KeyCode.Right);
+                    cki = Console.ReadKey();
+                }
+
+            } while (cki.Key != ConsoleKey.Escape && (GameModel.isAuthorized == true));
+
+
+        }
+    }
+}
