96e82f8afcfb283ecb5890f8800fc03fad363995
unknown <nuruddeensalihu@yahoo.com>
2015-07-23 13:28:22 +0800

    Adding a new class GameManager. GameMAnager Overseas the general request and commands from gameEngine. It manages the general game commands and request from the engine

---
+++ 2048_Console/gameManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048_Console
+{
+    class gameManager
+    {
+        private int[][] Tiles;
+        private TilesCl tile;
+        private DrawGame draw;
+        public gameManager(int[][] tiles)
+        {
+            Tiles = tiles;
+            tile = new TilesCl(tiles);
+            draw = new DrawGame(tiles);
+        }
+        public virtual void runGame(KeyCode key)
+        {
+            //Arrange the tiles  upward
+            tile.rearrangeTiles(key);
+            //merge the resulting tiles where similar
+            tile.mergeTiles(key);
+            //Rearrange the tiles 
+            tile.rearrangeTiles( key);
+            //Add New Tile
+            tile.addTile();
+            draw.writeTiles(Tiles);
+           // Console.Write(cki.Key.ToString());
+            //Renew KeyBoard Listener
+            
+        }
+       
+       
+        
+    }
+}
