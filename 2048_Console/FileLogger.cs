53cab7ab27ba6a2350300494e00eb8c8671143b3
unknown <nuruddeensalihu@yahoo.com>
2015-07-23 11:37:48 +0800

    Refactoring classes, adding FileLogger class

---
+++ 2048_Console/FileLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048_Console
+{
+    class FileLogger
+    {
+        public void Handle(string error)
+        {
+            System.IO.File.WriteAllText(System.AppDomain.CurrentDomain.BaseDirectory + "error.txt", error);
+
+        }
+    }
+}
