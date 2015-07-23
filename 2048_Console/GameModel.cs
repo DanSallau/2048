60d564fbf13f94eed0b0c5a84b8f16f1bf8208cc
unknown <nuruddeensalihu@yahoo.com>
2015-07-23 13:20:09 +0800

    Refacturing project classes to ensure good design and Object orientation .  A new Model Class GAmeModel is created responsible for holding the static model properties such as score and isGAmeOver

---
+++ 2048_Console/GameModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048_Console
+{
+    public class GameModel
+    {
+        public static int Score { get; set; }
+        public static bool isGameOver { get; set; }
+        public static bool isAuthorized { get; set; }
+    }
+}
